3289e3b933c4436f0c59c9acf1bc57a05e486077
James Tognola <togdem1@googlemail.com>
2026-06-18 18:54:12 +0100

    ...

--- Services/IConvertContactService.cs
+++ Services/IConvertContactService.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
-using System.Xml.Schema;
 using ContactToVCard.Helpers;
 
 namespace ContactToVCard.Services;
