1600a9a846520abf1b7ef9d26a8491170c639f7e
James Tognola <togdem1@googlemail.com>
2026-07-14 22:13:27 +0100

    Minor refactoring.

--- Converters/BoolToYesNoConverter.cs
+++ Converters/BoolToYesNoConverter.cs
@@ -6,17 +6,12 @@ namespace ContactToVCard.Converters;
 
 public class BoolToYesNoConverter : IValueConverter
 {
-    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        return value is true ? "Yes" : "No";
-    }
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) 
+        => value is true ? "Yes" : "No";
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is string text)
-        {
-            return string.Equals(text, "Yes", StringComparison.OrdinalIgnoreCase);
-        }
+        if (value is string text) return string.Equals(text, "Yes", StringComparison.OrdinalIgnoreCase);
 
         return false;
     }
